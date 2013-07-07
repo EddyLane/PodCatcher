b7b1ef3caa391a6e4bcf01545eee4abc76318325
Eddy Lane <Naedin@gmail.com>
2013-07-07 20:29:07 +0100

    Search redirect too

--- src/Podcast/MainBundle/Resources/public/app/controllers/podcast-search-controller.js
+++ src/Podcast/MainBundle/Resources/public/app/controllers/podcast-search-controller.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('podcatcher')
-    .controller('PodcastSearchCtrl', function ($scope, $http, limitToFilter, Podcast, User) {
+    .controller('PodcastSearchCtrl', function ($scope, $http, $location) {
+
         $scope.search = function(term) {
             return $http.get(Routing.generate('get_podcasts', { search: term, limit: 10 })).then(function(response){
-                return limitToFilter(response.data, 10);
+                return response.data;
             });
         };
+
+        $scope.onSelect = function($item) {
+            $location.path(Routing.generate('get_podcast', { slug: $item.slug }));
+        }
+
     });
