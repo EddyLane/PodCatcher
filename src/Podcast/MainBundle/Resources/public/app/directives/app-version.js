6a2a746fb1f998eeccd2e0461557bcd2ebbf83f5
Eddy Lane <Naedin@gmail.com>
2013-08-18 14:42:59 +0100

    threaded commentish

--- src/Podcast/MainBundle/Resources/public/app/directives/app-version.js
+++ src/Podcast/MainBundle/Resources/public/app/directives/app-version.js
@@ -19,6 +19,7 @@ directive('comments', function() {
 
         function getParameters() {
           return { 
+            // id: $scope.thread, 
             id: 'foo', 
             permalink: $window.location.href, 
             _format: 'json' 
@@ -26,11 +27,9 @@ directive('comments', function() {
         }
 
         function getPostParameters() {
-          var body = {
+          return angular.extend(csrfToken, {
             'fos_comment_comment[body]': $scope.commentBody
-          };
-          console.log(angular.extend(csrfToken, body));
-          return angular.extend(csrfToken, body);
+          });
         }
 
         function setReady() {
@@ -48,15 +47,13 @@ directive('comments', function() {
 
             $scope.form = false;
 
-            var comment = getPostParameters();
-
             $http({
               method: 'post',
               url: Routing.generate('fos_comment_post_thread_comments', getParameters()),
-              data: $.param(comment),
+              data: $.param(getPostParameters()),
               headers: {'Content-Type': 'application/x-www-form-urlencoded'}
             }).success(function() {
-              var successComment = {
+              $scope.comments.unshift({
                 comment: {
                   author: {
                     username: User.username
@@ -64,11 +61,15 @@ directive('comments', function() {
                   body: $scope.commentBody,
                   created_at: new Date().getTime()
                 }
-              };
-              $scope.comments.unshift(successComment);
+              });
               $scope.commentBody = '';
               $scope.form = true;
             });
+          },
+
+          reply: function(comment) {
+
+            comment.replying = !!(!comment.replying);
 
           },
 
@@ -98,4 +99,63 @@ directive('comments', function() {
 
     }]
   };
+})
+.directive('test', function($compile) {
+  // use an int to suffix scope properties 
+  // so that inheritance does not cause infinite loops anymore
+  var inc = 0;
+  return {
+    restrict: 'A',
+    compile: function(element, attr) {
+      // prepare property names
+      var prop = 'test'+(++inc),
+          childrenProp = 'children_'+prop,
+          labelProp = 'label'+prop,
+          childProp = 'child_'+prop;
+      
+      return function(scope, element, attr) {
+        // create a child scope
+        var childScope = scope.$new();
+        function observeParams() {
+          // eval attributes in current scope
+          // and generate html depending on the type
+          // console.log(childProp);
+          // console.log(childrenProp);
+          // console.log(labelProp);
+
+          console.log(attr);
+
+          var iTest = scope.$eval(attr.test),
+              iLabel = scope.$eval(attr.testLabel);
+        
+            if(iTest[0] && iTest[0] && iTest[0].comment) {
+              labelProp = iTest[0].comment.body;
+            }
+
+          var html = 
+              '<div>'+labelProp+'<ul><li ng-repeat="'+childProp+' in '+childrenProp+'"><div test="'+childProp+'.children" test-label="'+childProp+'.label">{{'+childProp+'}}</div></li></ul></div>'
+            ;
+
+
+          //  console.log(iTest);
+          
+          // set scope values and references
+          childScope[childrenProp]= iTest;
+          childScope[labelProp]= iLabel;
+          
+          // fill html
+          element.html(html);
+          
+          // compile the new content againts child scope
+          $compile(element.contents())(childScope);
+        }
+        
+        // set watchers
+        scope.$watch(attr.test, observeParams);
+        scope.$watch(attr.testLabel, observeParams);
+      };
+    }
+  };
 });
+
+
